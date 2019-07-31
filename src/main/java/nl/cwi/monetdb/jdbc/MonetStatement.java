79645695ee1ab660b67974fe897bec7bff14adea
Martin van Dinther <martin.van.dinther@monetdbsolutions.com>
2019-07-31 17:17:02 +0200

    Add a reference to the request logged in bugzilla to implement Statement.cancel() method

--- src/main/java/nl/cwi/monetdb/jdbc/MonetStatement.java
+++ src/main/java/nl/cwi/monetdb/jdbc/MonetStatement.java
@@ -308,6 +308,7 @@ public class MonetStatement
 	@Override
 	public void cancel() throws SQLException {
 		throw new SQLFeatureNotSupportedException("Query cancelling is currently not supported by the driver.", "0A000");
+		// a request to implement this is already logged, see: https://www.monetdb.org/bugzilla/show_bug.cgi?id=6222
 	}
 
 	/**
