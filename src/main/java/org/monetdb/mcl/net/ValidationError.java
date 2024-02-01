32d503b46df9629bc38347235c0d494c88de1d15
Martin van Dinther <martin.van.dinther@monetdbsolutions.com>
2024-02-01 17:11:45 +0100

    Suppress warning: [serial] serializable class ValidationError has no definition of serialVersionUID Add missing license header

--- src/main/java/org/monetdb/mcl/net/ValidationError.java
+++ src/main/java/org/monetdb/mcl/net/ValidationError.java
@@ -1,5 +1,18 @@
+/*
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0.  If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright 2024 MonetDB Foundation;
+ * Copyright August 2008 - 2023 MonetDB B.V.;
+ * Copyright 1997 - July 2008 CWI.
+ */
+
 package org.monetdb.mcl.net;
 
+@SuppressWarnings("serial")
 public class ValidationError extends Exception {
 	public ValidationError(String parameter, String message) {
 		super(parameter + ": " + message);
